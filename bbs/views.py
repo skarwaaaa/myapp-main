3bc7b585e941f83dbd53bb9e3002d0b638d8670d
skarwaaaa <skarwaaaa@gmail.com>
2021-05-06 11:10:29 +0900

    リポジトリ統合

--- bbs/views.py
+++ bbs/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'bbs/index.html')
+    context = {
+        'message': 'Welcome my BBS',
+        'players': ['勇者', '戦士', '魔法使い', '忍者']
+    }
+    return render(request, 'bbs/index.html', context)
